73609a781e3f7fc1cb2fb970a202ab6928acea4f
ricky16739 <56608853+ricky16739@users.noreply.github.com>
2019-11-07 16:03:16 -0500

    Update Student.h

--- Student.h
+++ Student.h
@@ -1,1 +1,14 @@
 //Student.h
+lass Student {
+    public:
+       Student(string initialName = "No Name") {
+             name = initialName;
+       }
+
+       virtual void ToStr(){
+            cout << name << " is undergraduate student" << endl;
+       }
+
+    protected:
+      string name;
+};
