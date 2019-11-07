d3592b9919a5e69dd1b29d6b873d10b4b2f5b8c7
Jordan Taylor <46736136+Jattaylor@users.noreply.github.com>
2019-11-07 16:37:08 -0500

    Update Professor.h

--- Professor.h
+++ Professor.h
@@ -1,1 +1,19 @@
 //Professor.h
+#include<iostream>
+#include"School.h"
+using namespace std;
+
+#ifndef PROFESSOR_H
+#define PROFESSOR_H
+class Professor: public School{
+	
+	private:
+	string tenure;
+	
+	public:
+	
+	Professor();
+	void setTenure(string);
+	void print();
+};
+#endif
