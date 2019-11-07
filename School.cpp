e9454ac46d5456545220f6cdbc39a3891d7492d0
Jordan Taylor <46736136+Jattaylor@users.noreply.github.com>
2019-11-07 15:57:57 -0500

    Update School.cpp

--- School.cpp
+++ School.cpp
@@ -1,1 +1,42 @@
 //School.cpp
+#include"School.h"
+#include<iostream>
+#include<string>
+
+School::School(){
+	
+	setName("", "");
+	setAge(0);
+	setID("");
+}
+
+School::School(string first, string last, int age, string id){
+	
+	setName(first, last);
+	setAge(age);
+	setID(id);
+}
+void School::setName(string first, string last){
+	
+	first_name = first;
+	last_name = last;
+}
+
+void School::setAge(int age){
+	
+	if(age >= 0)
+	age = age;
+	else
+	age = 0;
+}
+
+void School::setID(string id){
+	
+	this ->id = id;
+}
+void School::print(){
+	
+	cout<<"Name:"<<first_name<<""<<last_name<<"/n"
+	<<"Age:"<<age<<"/n"
+	<<"ID:"<<id<<"/n";
+}
