634e793f50b6163837f28c7b0a67e2b58175f907
D'ajon Jackson <49109510+Dajon-Jackson@users.noreply.github.com>
2019-11-07 15:41:42 -0500

    Name Change
    Changed  references of "staff" to "faculty" since professors are also staff.

--- StayInSchool.cpp
+++ StayInSchool.cpp
@@ -3,7 +3,7 @@
 #include<string>
 #include"School.h"
 #include"Professor.h"
-#include"Staff.h"
+#include"Faculty.h"
 #include"Student.h"
 using namespace std;
 
@@ -16,41 +16,41 @@ int main(int argc, char** argv){
   do
   {
   system("CLS");
-  cout<< "Enter 1 for a student, 2 for a professor, 3 for staff, and 4 for exit: ";
+  cout<< "Enter 1 for a student, 2 for a professor, 3 for faculty, and 4 for exit: ";
   cin>>choice;
   if(choice == 1)
   {
-    Student* stu = new Student;
+    Student* s = new Student;
     cout<<"Enter the student's first name, last name, and age, and ID number: ";
     cin>>fname>>lname>>age>>id;
-    stu->setName(fname, lname);
-    stu->setAge(age);
-    stu->setID(id);
-    scPTR[counter] = stu;
+    s->setName(fname, lname);
+    s->setAge(age);
+    s->setID(id);
+    scPTR[counter] = s;
 
     counter++;
   }
   else
     if(choice == 2)
     {
-      Professor* pro = new Professor;
+      Professor* p = new Professor;
       cout<<"Enter the professor's first name, last name, and age, and ID number: ";
       cin>>fname>>lname>>age>>id;
-      pro->setName(fname, lname);
-      pro->setAge(age);
-      pro->setID(id);
-      scPTR[counter] = pro;
+      p->setName(fname, lname);
+      p->setAge(age);
+      p->setID(id);
+      scPTR[counter] = p;
     }
     else
       if(choice == 3)
       {
-        Staff* sta = new Staff;
-        cout<<"Enter the staff member's first name, last name, and age, and ID number: ";
+    	Faculty* f = new Faculty;
+        cout<<"Enter the faculty member's first name, last name, and age, and ID number: ";
         cin>>fname>>lname>>age>>id;
-        sta->setName(fname, lname);
-        sta->setAge(age);
-        sta->setID(id);
-        scPTR[counter] = sta;
+        f->setName(fname, lname);
+        f->setAge(age);
+        f->setID(id);
+        scPTR[counter] = f;
       }
       else
         if(choice >4 || choice < 1)
