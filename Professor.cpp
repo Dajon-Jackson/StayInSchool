575b3c7cbe973fdca50cf1201111fa1fc7c3df79
Jordan Taylor <46736136+Jattaylor@users.noreply.github.com>
2019-11-07 16:37:39 -0500

    Update Professor.cpp

--- Professor.cpp
+++ Professor.cpp
@@ -1,1 +1,19 @@
 //Professor.cpp
+#include"Professor.h"
+
+Professor::Professor(){
+	setTenure("Teacher");
+	
+}
+void Professor::setTenure(string tenure){
+	
+	this ->tenure = tenure;
+	
+}
+void Professor::print(){
+	
+	cout<<"this professor has been working here for"<<tenure<<"year(s)"<<"\n"
+	<<"Name:"<<name<<"\n"
+	<<"Age:"<<age<<"\n"
+	<<"ID:"<<id<<"\n"
+}
